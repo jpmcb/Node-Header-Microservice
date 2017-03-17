3b12d4057f70c6a95dec71c97d7267535e7b8fdf
John McBride <jpmmcbride@gmail.com>
2017-03-17 04:15:59 +0000

    Update server with proper API and add readme

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # Node API Header Microservice
 _____________
 
-Placeholder
+Simple API that provides request with a JSON object that contains the IPv4, IPv6, language, and operating system of requesting cliet. Running version of the app can be found [here](https://header-microservice-api.herokuapp.com/).
--- server.js
+++ server.js
@@ -11,26 +11,79 @@ var speed = os.arch();
 
 http.createServer(function(req, res) {
     
-    if(req.url === '/') {
+    console.log(req.headers);
+    
+    // SERVER RESPONSES
+    if(req.url === '/server') {
         var lang = req.headers['accept-language'].split(',')[0];
         
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{IPv4: ${ipv4}, IPv6: ${ipv6}, Language: ${lang}, System: ${system} ${release} ${speed}}`);
-    } else if(req.url === '/ip') {
+    } else if(req.url === '/server/ip') {
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{IPv4: ${ipv4}, IPv6: ${ipv6}}`);   
-    } else if (req.url === '/ipv4') {
+    } else if (req.url === '/server/ipv4') {
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{IPv4: ${ipv4}}`);
-    } else if (req.url === '/ipv6') {
+    } else if (req.url === '/server/ipv6') {
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{IPv6: ${ipv6}}`);
-    } else if (req.url === '/lang') {
+    } else if (req.url === '/server/lang') {
         var lang = req.headers['accept-language'].split(',')[0];
         
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{Language: ${lang}}`);
-    } else if (req.url === '/os') {
+    } else if (req.url === '/server/os') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{System: ${system} ${release} ${speed}}`);
+    
+        
+        // TO CHANGE - USER RESPONSES
+    } else if(req.url === '/user') {
+        var lang = req.headers['accept-language'].split(',')[0];
+        
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IP: ${req.headers['x-forwarded-for']}, Language: ${lang}, System: ${req.headers['user-agent']} }`);
+    } else if(req.url === '/user/ip') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IP: ${ipv4}}`); //finished   
+    } else if (req.url === '/server/ipv4') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv4: ${ipv4}}`);
+    } else if (req.url === '/server/ipv6') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv6: ${ipv6}}`);
+    } else if (req.url === '/server/lang') {
+        var lang = req.headers['accept-language'].split(',')[0];
+        
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{Language: ${lang}}`);
+    } else if (req.url === '/server/os') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{System: ${system} ${release} ${speed}}`);
+    
+        
+        // COMBINED RESPONSES
+    } else if(req.url === '/') {
+        var lang = req.headers['accept-language'].split(',')[0];
+        
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv4: ${ipv4}, IPv6: ${ipv6}, Language: ${lang}, System: ${system} ${release} ${speed}}`);
+    } else if(req.url === '/server/ip') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv4: ${ipv4}, IPv6: ${ipv6}}`);   
+    } else if (req.url === '/server/ipv4') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv4: ${ipv4}}`);
+    } else if (req.url === '/server/ipv6') {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{IPv6: ${ipv6}}`);
+    } else if (req.url === '/server/lang') {
+        var lang = req.headers['accept-language'].split(',')[0];
+        
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(`{Language: ${lang}}`);
+    } else if (req.url === '/server/os') {
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(`{System: ${system} ${release} ${speed}}`);
     }
